bcbd37dea7a82fb838b9acb230ba1fe9212b9154
Ryan Yang <y.ryan.yang@gmail.com>
2023-02-13 12:38:18 -0500

    setup

--- network.py
+++ network.py
@@ -1,2 +1,22 @@
 class Network:
-    
+    nodes = []
+    links = []
+    pipes = []
+
+# class Node:
+#     #source/destination
+
+class Link:
+    capacity = 
+    total_current =
+    set_passing = 
+    set_active = 
+
+class Tunnel: #pipes travelling along the same path
+    src = 
+    dest
+
+class Pipe:
+    src = 
+    dest = 
+    num_connections = 
