162b2faec1395064f926fbb9b6e5428a995ef0e9
Ryan Yang <y.ryan.yang@gmail.com>
2023-02-13 13:20:42 -0500

    setup

--- network.py
+++ network.py
@@ -1,16 +1,18 @@
 class Network:
-    def __init__(self, nodes, links, tunnels, pipes):
-        self.nodes = []
-        self.links = []
-        self.tunnels = []
-        self.pipes = []
+    def __init__(self, nodes, links, pipes, tunnels=-1):
+        self.nodes = nodes
+        self.links = links
+        self.pipes = pipes
+        self.tunnels = tunnels
 
 class Link:
-    def __init__(self, capacity, total_throughput, pipes_total, pipes_active):
+    def __init__(self, capacity, start, stop, total_throughput = 0, pipes_total = [], pipes_active = []):
         self.capacity = capacity
-        self.total_throughput = 0
-        self.set_passing = []
-        self.set_active = []
+        self.start = start
+        self.stop = stop
+        self.total_throughput = total_throughput
+        self.pipes_total = pipes_total
+        self.pipes_active = pipes_active
 
 class Tunnel: #pipes travelling along the same path
     def __init__(self, src, dst):
@@ -18,8 +20,28 @@ class Tunnel: #pipes travelling along the same path
         self.dst = dst
 
 class Pipe:
-    def __init__(self, src, dst, tunnel_index, num_connections=0):
+    def __init__(self, src, dst, tunnel_index=-1, num_connections=0):
         self.src = src
         self.dst = dst
         self.tunnel_index = tunnel_index
-        num_connections = 0
+        self.num_connections = num_connections
+
+
+def naive_water_filling():
+
+
+def main():
+    #doing two pipes for now - ryan (1pm)
+    link1 = Link(capacity = 10, 0, 1)
+    link2 = Link(capacity = 20, 1, 2)
+    link3 = Link(capacity = 15, 2, 3)
+
+    pipe1 = Pipe(0, 2)
+    pipe2 = Pipe(1,3)
+    network1 = Network([0,1,2,3], [link1, link2, link3],[pipe1, pipe2] )
+    naive_water_filling(network1)
+
+
+
+if __name__ == "__main__":
+    main()
